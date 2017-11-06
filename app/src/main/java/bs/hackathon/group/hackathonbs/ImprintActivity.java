dbb440868c45c913a86d8d69486be20f0ce2d2d9
jokefredev <jokefre.dev@gmail.com>
2017-11-06 15:59:15 +0100

    intial app setup imprint activity fix

---
+++ app/src/main/java/bs/hackathon/group/hackathonbs/ImprintActivity.java
@@ -0,0 +1,13 @@
+package bs.hackathon.group.hackathonbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imprint);
+    }
+}
