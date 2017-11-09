907a1894aa0e33a54510f00f8ddfdc5d35aec86a
ralphkurche <33416851+ralphkurche@users.noreply.github.com>
2017-11-09 18:40:35 +0100

    Profil und so

---
+++ app/src/main/java/bs/hackathon/group/hackathonbs/ProfileActivity.java
@@ -0,0 +1,25 @@
+package bs.hackathon.group.hackathonbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import bs.hackathon.group.hackathonbs.Adapter.CustomAdapter;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ListView categoryListView;
+    private String[] categories = {"Ernährung", "Mobilität", "Betreuung", "Bildung"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        categoryListView = findViewById(R.id.listview_category);
+
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(),categories);
+        categoryListView.setAdapter(customAdapter);
+    }
+}
